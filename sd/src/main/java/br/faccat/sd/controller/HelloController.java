09a32dffd0c76827590a3ae8373a7fed4a68f939
Rodrigo Frey <rodrigo@infisc.com.br>
2019-12-03 23:08:15 -0300

    ajustados" "

---
+++ sd/src/main/java/br/faccat/sd/controller/HelloController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.faccat.sd.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author rodrigo
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    String getHello(@RequestParam(defaultValue = "", required = false) String nome,
+            HttpServletRequest req,
+            HttpServletResponse res) throws UnknownHostException {
+
+        StringBuilder sb = new StringBuilder();
+
+        InetAddress inetAddress = InetAddress.getLocalHost();
+
+        sb.append("&nbsp;IP Address:- ").append(inetAddress.getHostAddress());
+        sb.append("&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Host Name:- ").append(inetAddress.getHostName());
+        if (nome == null || "".equals(nome) ) {
+            sb.append("&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Hello World");
+        } else {
+            sb.append("&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Seja bem vindo ").append(nome);
+        }
+
+        return sb.toString();
+    }
+}
---
+++ sd/src/test/java/br/faccat/sd/SdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faccat.sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
